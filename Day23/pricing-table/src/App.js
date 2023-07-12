fc7e2a71edd7126d305d2e5f57d683af33829a62
Yuktee <yuktee.@example.com>
2023-07-13 01:08:51 +0530

    added 23rd day task

---
+++ Day23/pricing-table/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+import PricingTable from './PricingTable';
+
+function App() {
+  return (
+    <div className="App">
+      <PricingTable />
+    </div>
+  );
+}
+
+export default App;
